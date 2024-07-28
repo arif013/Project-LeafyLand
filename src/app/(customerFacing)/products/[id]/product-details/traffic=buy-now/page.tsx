9d7418bd879ba240fc6ea85f1341e805fe263d7b
arif013 <mdarif11099@gmail.com>
2024-07-29 01:18:24 +0530

    Payment gateway added

--- src/app/(customerFacing)/products/[id]/product-details/traffic=buy-now/page.tsx
+++ src/app/(customerFacing)/products/[id]/product-details/traffic=buy-now/page.tsx
@@ -1,3 +1,11 @@
-export default function BuyNow(){
+import db from "@/db/db"
+import { notFound } from "next/navigation"
+
+export default async function BuyNow({ params: {id} }: {params: {id: string}}){
+    const product = await db.product.findUnique({
+        where: { id }
+    })
+    if(product == null) return notFound()
+        
     return <div>Hi</div>
 }
